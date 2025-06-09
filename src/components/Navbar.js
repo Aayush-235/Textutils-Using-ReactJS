5988d6fee97758645322f5be12c04fcb952bed6d
Savaliya Aayush Rameshbhai <aayushsavaliya235@gmail.com>
2025-06-09 12:28:44 +0530

    After 21 Video

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,16 +14,16 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
             </li>
           </ul>
-          {/* <form className="d-flex" role="search">
+          <form className="d-flex mx-3" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-secondary btn btn-primary text-white" type="submit">Search</button>
-          </form> */}
+          </form>
 
         <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
             <input className="form-check-input" onClick={props.toggleMode}type="checkbox" role="switch" id="switchCheckDefault" />
